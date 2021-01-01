0dfe6d0ef183e8e280c270a00f9108e2fa340ce7
adavize <adavize@thepoint.africa>
2021-01-01 19:11:04 +0100

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Adavize Ozorku on 01/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Adavize Ozorku on 01/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
